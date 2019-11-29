4e43d8d3f3ca9182bd39b2d6ae2ccafe3478d31d
Ganesh Ravi Shankar <ganesh@upshotly.com>
2019-11-29 15:02:52 +0530

    Sample SRIT App

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('<h1>Welcome SRIT!</h1>');
+});
+
+var server = app.listen(3000, function () {
+  console.log('App started successfully. Listening to pot 3000');
+});
+
